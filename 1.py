e4b733bd07bd51cf37c4edf7cfae5a02949af870
OrangeTree11 <122359087+OrangeTree11@users.noreply.github.com>
2023-04-27 17:49:09 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,3 @@
+inp_name = input('당신의 이름은 무엇입니까? ')
+fav_fruit = input('당신은 어떤 과일을 좋아합니까? ')
+print(inp_name + '씨가 좋아하는 과일은 ' + fav_fruit + '입니다!')
---
+++ 2.py
@@ -0,0 +1,6 @@
+n, x = map(int, input().split())
+num_list = list(map(int, input().split()))
+
+for i in range(n):
+    if num_list[i] > x:
+        print(num_list[i], end=" ")
